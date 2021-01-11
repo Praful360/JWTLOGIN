a932ae5c6d7d2fd048e18d80ffb07ce7c2709dbd
Praful360 <53175654+Praful360@users.noreply.github.com>
2021-01-11 22:00:27 +0545

    ok

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+Route::group([
+
+    'prefix' => 'auth'
+
+], function () {
+
+    Route::post('login', 'App\Http\Controllers\AuthController@login');
+    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+    Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+    Route::post('me', 'App\Http\Controllers\AuthController@me');
+
+});
